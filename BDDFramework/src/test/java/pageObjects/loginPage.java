12c6cba0b741368fd6f584f5ee45899a0406176a
Julio Lima <61190307+julius-lima@users.noreply.github.com>
2024-02-26 14:33:10 -0300

    Adicionado via Upload

---
+++ BDDFramework/src/test/java/pageObjects/loginPage.java
@@ -0,0 +1,44 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class loginPage {
+WebDriver driver;
+By txt_username = By.id("user-name");
+By txt_password = By.id("password");
+By btn_login = By.id("login-button");
+By lbl_logo = By.xpath("//div[@class='app_logo']");
+
+public loginPage(WebDriver driver)
+{
+this.driver = driver;
+}
+public void enterUsername(String username)
+{
+	driver.findElement(By.id("user-name")).sendKeys(username);	
+}
+
+public void enterPassword(String password)
+{
+	driver.findElement(By.id("password")).sendKeys(password);
+}
+
+public void clickLogin()
+{
+	driver.findElement(By.id("login-button")).click();
+}
+
+public void isLogoDisplayed()
+{
+	driver.findElement(By.id("lbl_logo")).isDisplayed();
+}
+
+public void userLogin (String username, String password)
+{
+	driver.findElement(By.id("user-name")).sendKeys(username);
+	driver.findElement(By.id("password")).sendKeys(password);
+	driver.findElement(By.id("login-button")).click();
+}
+
+}
---
+++ BDDFramework/target/test-classes/Features/login.feature
@@ -0,0 +1,12 @@
+Feature: Login page Automation of saucedemo App
+
+  Scenario Outline: Check login is sucessful with valid creds
+    Given User is on login page
+    When User enters valid <"username"> and <"password">
+    And Clicks on Login Button
+    Then User is navegated to Home Page
+    And Close the Browser
+
+    Examples: 
+      | username      | password     |
+      | standard_user | secret_sauce |
